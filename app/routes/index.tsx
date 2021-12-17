52de50df3d36254aafc7a7b93ae92e7f2f696960
Komefumi <komefumi@protonmail.ch>
2021-12-17 19:08:09 +0530

    1. basic index route 2. nested post route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,18 @@
 function Home() {
   return (
     <div>
-      <h1>Welcome, for now</h1>
+      <h1>Fine, welcome to Remix</h1>
+      <div>
+        <p>
+          Remix is a convenient, full stack web framework made by the creators
+          of react router.
+        </p>
+        <p>
+          This simple blog app is being built by following Brad Traversy of
+          TraversyMedia, and is being built with subtle differences and could be
+          used as a starting point in future development
+        </p>
+      </div>
     </div>
   );
 }
