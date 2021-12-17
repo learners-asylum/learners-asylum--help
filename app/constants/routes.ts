cceb01f5303e51e52fd3fface3625b1c19b71730
Komefumi <komefumi@protonmail.ch>
2021-12-17 18:59:48 +0530

    1. remove existant styling 2. remove existant routes 3. use a new set of global styles

---
+++ app/constants/routes.ts
@@ -0,0 +1,4 @@
+export default {
+  HOME: "/",
+  POSTS: "/posts",
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,18 @@
-import { Outlet, LiveReload } from "remix";
-import { TitledWrapperProps } from "~/types/props";
+import { Outlet, LiveReload, Link, Links } from "remix";
+import { TitledWrapperProps, WrapperProps } from "~/types/props";
+import ROUTES from "~/constants/routes";
+import globalStylesUrl from "~/styles/global.css";
+
+const { HOME, POSTS } = ROUTES;
+
+export const links = () => [{ rel: "stylesheet", href: globalStylesUrl }];
 
 export default function App() {
   return (
     <Document title="My Remix Blog">
-      <Outlet />
+      <Layout>
+        <Outlet />
+      </Layout>
     </Document>
   );
 }
@@ -13,6 +21,8 @@ function Document({ children, title }: TitledWrapperProps) {
   return (
     <html lang="en">
       <head>
+        <Links />
+        <link rel="stylesheet" href={globalStylesUrl} />
         <title>{title}</title>
       </head>
       <body>
@@ -20,5 +30,24 @@ function Document({ children, title }: TitledWrapperProps) {
         {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
+  );
+}
+
+function Layout({ children }: WrapperProps) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to={HOME} className="logo">
+          Remix
+        </Link>
+        <ul className="nav">
+          <li>
+            <Link to={POSTS}>Posts</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="container">{children}</div>
+    </>
   );
 }
